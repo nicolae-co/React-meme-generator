ebacbc4b1fb21d32edb531c55ff5de0011ac5d9e
nicolae-co <cojocarugithub@gmail.com>
2023-01-19 21:40:46 +0200

    Inputs

---
+++ vite-project/src/components/Meme.jsx
@@ -0,0 +1,13 @@
+function Meme() {
+    return (
+        <main>
+        <form className="form">
+                <input className="form--input" type="text" placeholder="Top text"/>
+                <input className="form--input" type="text" placeholder="Bottom text"/>
+                <button className="form--button" type="button">Get a new meme image  🖼</button>
+            </form>
+        </main>
+    )
+}
+
+export default Meme
