2bbebceef209f8675667498606d2b79f6caf40e0
nicolae-co <cojocarugithub@gmail.com>
2023-01-20 23:58:50 +0200

    Event listner

--- vite-project/src/components/Meme.jsx
+++ vite-project/src/components/Meme.jsx
@@ -1,11 +1,36 @@
+import memeData from "../data"
+
 function Meme() {
+
+    
+
+    function getMemeImage() {
+        const memeArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memeArray.length)
+        const url = memeArray[randomNumber].url
+    }
+
     return (
         <main>
-        <form className="form">
-                <input className="form--input" type="text" placeholder="Top text"/>
-                <input className="form--input" type="text" placeholder="Bottom text"/>
-                <button className="form--button" type="button">Get a new meme image  🖼</button>
-            </form>
+            <div className="form">
+                <input
+                    className="form--input"
+                    type="text"
+                    placeholder="Top text"
+                />
+                <input
+                    className="form--input"
+                    type="text"
+                    placeholder="Bottom text"
+                />
+                <button
+                    onClick={getMemeImage}
+                    className="form--button"
+                    type="button"
+                >
+                    Get a new meme image  🖼
+                </button>
+            </div>
         </main>
     )
 }
