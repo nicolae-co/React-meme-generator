6a712cdd9b2458ac18ab768131556004e58d9a8b
Nicolae <106536189+nicolae-co@users.noreply.github.com>
2023-01-26 15:41:49 +0200

    Set text to meme

--- vite-project/src/components/Meme.jsx
+++ vite-project/src/components/Meme.jsx
@@ -21,6 +21,15 @@ function Meme() {
         }))
     }
 
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+
+    }
+
     return (
         <main>
             <div className="form">
@@ -28,11 +37,18 @@ function Meme() {
                     className="form--input"
                     type="text"
                     placeholder="Top text"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+
                 />
                 <input
                     className="form--input"
                     type="text"
                     placeholder="Bottom text"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button
                     onClick={getMemeImage}
@@ -42,7 +58,11 @@ function Meme() {
                     Get a new meme image  🖼
                 </button>
             </div>
-            <img className="form-img" src={meme.randomImage} alt="img" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
